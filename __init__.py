fc925ec3cd5a910306f0b72e61f3e915c77c21f7
lngd <lngdeer@gmail.com>
2024-09-02 11:04:11 +0300

    0.0.0 module

---
+++ __init__.py
@@ -0,0 +1,9 @@
+# Navtex messages analyzing tool.
+# Very simple.
+# Uses Bag of Words method to store all alphabetic words to be recognized.
+# Treats almost all alphanumeric words as non-memorizable chunks.
+#
+# v 1.0.0 2024 jun 13
+#
+# author: lngd
+# lngdeer@gmail.com
