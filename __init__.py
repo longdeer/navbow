37e8a7c4c0c4d0ea2ba6ac04e0c6bcc1dbff4555
lngd <lngdeer@gmail.com>
2025-04-10 11:56:22 +0300

    analyze variant

--- __init__.py
+++ __init__.py
@@ -33,3 +33,45 @@ class Navanalyzer:
 
 
 
+
+if	__name__ == "__main__":
+
+	from collections				import defaultdict
+	from pygwarts.hagrid.walks		import fstree
+	from NavtexBoWAnalyzer.scanner	import byte_scan
+	from NavtexBoWAnalyzer.scanner	import sanit_scan
+
+	FOLDER	= ""
+	SYMBOLS	= set()
+	WORDS	= defaultdict(int)
+	BROKEN	= list()
+
+	for branch,folders,files in fstree(FOLDER):
+		for file in files:
+
+			if (broken := byte_scan(file)) is not None : BROKEN.append(broken)
+			else:
+
+				current  = sanit_scan(file)
+				SYMBOLS |= current["symbols"]
+
+				for chunk in current["chunks"]: WORDS[chunk] += 1
+
+			print(f"processed {file}")
+
+
+	with open("", "w") as dump:
+
+		print(f"{sorted(SYMBOLS)}", file=dump)
+		print(file=dump)
+		for broken in BROKEN : print(broken, file=dump)
+		print(file=dump)
+		for word,f in sorted(WORDS.items(), key=lambda E : E[1], reverse=True):
+			print(f"{word}: {f}", file=dump)
+
+
+
+
+
+
+
