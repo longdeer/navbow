6d5216d78ddd28e21b8405b37b94e3070b08ca40
lngd <lngdeer@gmail.com>
2025-04-15 13:21:57 +0300

    analyzer final prototype

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from NavtexBoWAnalyzer.header			import B1
 from NavtexBoWAnalyzer.header			import B2
 from NavtexBoWAnalyzer.header			import G_NAVTEX_MESSAGE_HEADER
-from NavtexBoWAnalyzer.coordinates		import G_COORDINATE
-from NavtexBoWAnalyzer.numerical		import G_NUMERICAL
-from NavtexBoWAnalyzer.alphanumerical	import G_ALPHANUMERICAL
+from NavtexBoWAnalyzer.coordinates		import P_COORDINATE
+from NavtexBoWAnalyzer.numerical		import P_NUMERICAL
+from NavtexBoWAnalyzer.alphanumerical	import P_ALPHANUMERICAL
 from NavtexBoWAnalyzer.scanner			import byte_scan
 from NavtexBoWAnalyzer.scanner			import sanit_scan
 
@@ -53,26 +53,19 @@ if	__name__ == "__main__":
 	from	pygwarts.hagrid.walks		import fstree
 
 	ROOT	= ""
-	FOLDER1	= ""
-	FOLDER2	= ""
-	FOLDER3	= ""
-	FOLDER4	= ""
-	FOLDER5	= ""
+	TARGET	= ""
 	SYMBOLS	= set()
 	ARCHIVE = dict()
-	WORDS	= defaultdict(int)
 	BROKEN	= list()
-	ERROR	= list()
+	ERRORS	= list()
 	WRONGS	= list()
-	ALPHAS	= defaultdict(int)
 	NUMBS	= defaultdict(int)
 	ALNUMBS	= defaultdict(int)
 	OTHER	= defaultdict(int)
 	COORD	= defaultdict(int)
-	AWORD	= re.compile(r"\(?[A-Z]+[\-/]?[A-Z]+\)?[\-\.,:]?")
 
-	for sprout in [ fstree(FOLDER1), fstree(FOLDER2), fstree(FOLDER3), fstree(FOLDER4), fstree(FOLDER5) ]:
-		for branch, folders, files in sprout:
+	for Y in range(2013,2025):
+		for branch, folders, files in fstree(os.path.join(ROOT,str(Y))):
 			for file in files:
 
 				if (broken := byte_scan(file)) is not None : BROKEN.append(broken)
@@ -85,7 +78,7 @@ if	__name__ == "__main__":
 					eos		= current["air_lines"][-1]
 					body	= current["chunks"] - set(header.split()) - { "NNNN" }
 
-					if "*" in current["symbols"]: ERROR.append(message)
+					if "*" in current["symbols"]: ERRORS.append(message)
 					elif(
 
 						not G_NAVTEX_MESSAGE_HEADER.fullmatch(header)
@@ -101,60 +94,51 @@ if	__name__ == "__main__":
 						with open(good_path, "w") as good_file : good_file.write(message)
 
 						for chunk in body:
-							WORDS[chunk] += 1
 
-							if		G_COORDINATE.fullmatch(chunk): COORD[chunk] += 1
+							if		P_COORDINATE.fullmatch(chunk): COORD[chunk] += 1
 							elif	G_NUMERICAL.fullmatch(chunk): NUMBS[chunk] += 1
-							elif	G_ALPHANUMERICAL.fullmatch(chunk): ALNUMBS[chunk] += 1
-							elif	AWORD.fullmatch(chunk): ALPHAS[chunk] += 1
+							elif	P_ALPHANUMERICAL.fullmatch(chunk): ALNUMBS[chunk] += 1
 							else:	OTHER[chunk] += 1
 
 				print(f"processed {file}")
 
 
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "1-sb-scan"), "w") as dump:
 
 		print(f"{sorted(SYMBOLS)}", file=dump)
 		print(file=dump)
 		print(f"brokens:", file=dump)
 		for broken in BROKEN : print("\n".join(broken), file=dump)
 		print(file=dump)
-		for word,f in sorted(WORDS.items(), key=lambda E : E[1], reverse=True):
-			print(f"{word}: {f}", file=dump)
 
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "2-errors-scan"), "w") as dump:
 
 		print(f"errors:", file=dump)
-		for error in ERROR : print(error + "\n", file=dump)
+		for error in ERRORS : print(error + "\n", file=dump)
 		print(file=dump)
 
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "3-wrongs-scan"), "w") as dump:
 
 		print(f"wrongs:\n", file=dump)
 		for wrong in WRONGS : print(wrong + "\n", file=dump)
 		print(file=dump)
 
-	with open("", "w") as dump:
-
-		for word,f in sorted(ALPHAS.items(), key=lambda E : E[1], reverse=True):
-			print(f"{word}: {f}", file=dump)
-
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "4-numbs-scan"), "w") as dump:
 
 		for word,f in sorted(NUMBS.items(), key=lambda E : E[1], reverse=True):
 			print(f"{word}: {f}", file=dump)
 
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "5-alnums-scan"), "w") as dump:
 
 		for word,f in sorted(ALNUMBS.items(), key=lambda E : E[1], reverse=True):
 			print(f"{word}: {f}", file=dump)
 
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "6-others-scan"), "w") as dump:
 
 		for word,f in sorted(OTHER.items(), key=lambda E : E[1], reverse=True):
 			print(f"{word}: {f}", file=dump)
 
-	with open("", "w") as dump:
+	with open(os.path.join(TARGET, "7-coords-scan"), "w") as dump:
 
 		for word,f in sorted(COORD.items(), key=lambda E : E[1], reverse=True):
 			print(f"{word}: {f}", file=dump)
